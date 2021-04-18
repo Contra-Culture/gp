e7ca9f14d231f6a36c6ef2fd0908658f63c10fef
Vladimir Melnick <egotraumatic@gmail.com>
2021-04-18 05:18:41 +0300

    test fix

--- gp_test.go
+++ gp_test.go
@@ -10,7 +10,7 @@ var _ = Describe("gp", func() {
 	Context("creation", func() {
 		Describe("New()", func() {
 			It("returns parser node", func() {
-				pn := gp.New("test purpose", gp.ExactTokenParser("test", "test"))
+				pn := gp.New("test purpose", gp.Optional(false), gp.ExactTokenParser("test", "test"))
 				Expect(pn).NotTo(BeNil())
 			})
 		})
